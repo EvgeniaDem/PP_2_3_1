168c0717b673f2c5bce2f510377b883f7f6cae88
Евгения <jane_79@list.ru>
2022-08-19 18:14:37 +0300

    2nd commit

--- src/main/java/application/controller/UserController.java
+++ src/main/java/application/controller/UserController.java
@@ -1,4 +1,7 @@
 package application.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
 }
--- src/main/java/application/dao/UserDaoImpl.java
+++ src/main/java/application/dao/UserDaoImpl.java
@@ -1,4 +1,7 @@
 package application.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDaoImpl {
 }
