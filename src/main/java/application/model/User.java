74be3ff81c34c2766666ca58c64da45a8a27b1e8
Евгения <jane_79@list.ru>
2022-08-19 15:57:30 +0300

    1st commit

---
+++ src/main/java/application/model/User.java
@@ -0,0 +1,18 @@
+package application.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public User() {
+    }
+}
